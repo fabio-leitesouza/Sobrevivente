49b98a51d5de68deff776af9beb58a421f0e3b53
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-04-04 17:24:58 -0300

    update

--- Assets/Script/ControlaInimigo.cs
+++ Assets/Script/ControlaInimigo.cs
@@ -21,10 +21,13 @@ public class ControlaInimigo : MonoBehaviour
     {
         float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
 
-        if(distancia > 2)
+        if(distancia < 10)
         {
             Vector3 direcao = Jogador.transform.position - transform.position;
             GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao * Velocidade * Time.deltaTime);
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
         }
 
         
