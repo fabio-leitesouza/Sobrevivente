aa24bdd4a7321fffd339393abf7e01fbec411ec8
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-04-09 14:23:16 -0300

    update

--- Assets/Script/ControlaJogador.cs
+++ Assets/Script/ControlaJogador.cs
@@ -6,6 +6,7 @@ public class ControlaJogador : MonoBehaviour
 {
     public float Velocidade = 10;
     private Vector3 direcao;
+    public LayerMask MascaraChao;
 
     // Update is called once per frame
     void Update()
@@ -27,6 +28,21 @@ public class ControlaJogador : MonoBehaviour
     }
     void FixedUpdate()
     {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * Velocidade * Time.deltaTime));
+        GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position + 
+            (direcao * Velocidade * Time.deltaTime));
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(raio.origin, raio.direction, Color.red);
+
+        RaycastHit impacto;
+        
+        if(Physics.Raycast(raio, out impacto, 20, MascaraChao))
+        {
+            Vector3 posicaoMiraJogador = impacto.point - transform.position;
+            posicaoMiraJogador.y = transform.position.y;
+
+            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
     }
 }
